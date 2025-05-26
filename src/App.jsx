dcd2f7a16a6e7462562ea2fb4c91bfd61b07ef99
Keisha <keisha23si@mahasiswa.pcr.ac.id>
2025-05-26 14:05:32 +0700

    starter project sia by kva

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import MainLayout from './components/MainLayout'
+import Dashboard from './pages/Dashboard'
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
