4726ef567188df77de4e04e0f16b497d140a1ed1
baim <ibrahim23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:31 +0700

    penjualan by baim

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 import Dashboard from './pages/Dashboard'
+import SalesManagement from './pages/SalesManagement'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path='/' element={<Dashboard />} />
+           <Route path='Penjualan' element={<SalesManagement />} />
         </Route>
       </Routes>
     </>
