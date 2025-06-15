db1a1cfc1a5129964272a82d786cc5457dc0c61d
Keisha <keisha23si@mahasiswa.pcr.ac.id>
2025-06-15 23:37:34 +0700

    perubahan sidebar- kva

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,6 +9,7 @@ import {
   UserPlus,
 } from 'lucide-react';
 import { Link, useLocation } from 'react-router-dom';
+import "./color.css";
 
 const menuItems = [
   { name: 'Dashboard', icon: <LayoutDashboard />, path: '/' },
@@ -31,16 +32,16 @@ const Sidebar = () => {
 
   return (
     <aside className="bg-white w-64 h-screen shadow-lg px-4 py-6 hidden md:block overflow-y-auto">
-      <div className="text-xl font-bold mb-8 text-purple-700">Century</div>
+     <img  className="block mx-auto mb-4 w-35 ml-1 h-auto relative -top-2"  src="public/logo2.png" alt="logo" />
       <nav className="space-y-1">
         {menuItems.map((item) => (
           <Link
             key={item.name}
             to={item.path}
-            className={`flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-purple-100 transition ${
+            className={`flex items-center gap-3 px-3 py-2 rounded-lg transition ${
               isActive(item.path)
-                ? 'bg-purple-200 text-purple-800 font-semibold'
-                : 'text-gray-700'
+                ? 'bg-tosca-200 text-black font-semibold'
+                : 'text-gray-700 hover-bg-tosca-100'
             }`}
           >
             <span className="w-5 h-5">{item.icon}</span>
@@ -49,7 +50,7 @@ const Sidebar = () => {
         ))}
       </nav>
 
-      <div className="mt-8 text-xs font-semibold text-gray-500">AKUN</div>
+      <div className="mt-70 text-xs font-semibold text-gray-500">AKUN</div>
       <nav className="mt-2 space-y-1">
         {accountItems.map((item) => (
           <Link
